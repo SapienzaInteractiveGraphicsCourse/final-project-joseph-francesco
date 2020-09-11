4a4acbcf6075f3a4985af1e2b65371dc2f01e122
Joseph Aguilar <joseph.feener@taggo.io>
2020-09-11 20:29:17 +0200

    assets folder added

--- js/index.js
+++ js/index.js
@@ -131,50 +131,62 @@ function handleTouchEnd(event){
 
 
 var Sea = function(){
-  var geom = new THREE.CylinderGeometry(game.seaRadius,game.seaRadius,game.seaLength,40,10);
-  geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
-  geom.mergeVertices();
-  var l = geom.vertices.length;
 
-  this.waves = [];
+	var loader = new THREE.TextureLoader();
+	loader.load( 'assets/grass.png', function ( texture ) {
 
-  for (var i=0;i<l;i++){
-	var v = geom.vertices[i];
-	//v.y = Math.random()*30;
-	this.waves.push({y:v.y,
-					 x:v.x,
-					 z:v.z,
-					 ang:Math.random()*Math.PI*2,
-					 amp:game.wavesMinAmp + Math.random()*(game.wavesMaxAmp-game.wavesMinAmp),
-					 speed:game.wavesMinSpeed + Math.random()*(game.wavesMaxSpeed - game.wavesMinSpeed)
-					});
-  };
-  var mat = new THREE.MeshPhongMaterial({
-	color:Colors.blue,
-	transparent:true,
-	opacity:.8,
-	shading:THREE.FlatShading,
+		var geom = new THREE.SphereGeometry( 200, 20, 20 );
 
-  });
+		var material = new THREE.MeshBasicMaterial( { map: texture, overdraw: 0.5 } );
+		var mesh = new THREE.Mesh( geom, material );
+		// group.add( mesh );
 
-  this.mesh = new THREE.Mesh(geom, mat);
-  this.mesh.name = "waves";
-  this.mesh.receiveShadow = true;
+	} );
+
+  // var geom = new THREE.CylinderGeometry(game.seaRadius,game.seaRadius,game.seaLength,40,10);
+  // geom.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+  // geom.mergeVertices();
+  // var l = geom.vertices.length;
+  //
+  // this.waves = [];
+  //
+  // for (var i=0;i<l;i++){
+	// var v = geom.vertices[i];
+	// //v.y = Math.random()*30;
+	// this.waves.push({y:v.y,
+	// 				 x:v.x,
+	// 				 z:v.z,
+	// 				 ang:Math.random()*Math.PI*2,
+	// 				 amp:game.wavesMinAmp + Math.random()*(game.wavesMaxAmp-game.wavesMinAmp),
+	// 				 speed:game.wavesMinSpeed + Math.random()*(game.wavesMaxSpeed - game.wavesMinSpeed)
+	// 				});
+  // };
+  // var mat = new THREE.MeshPhongMaterial({
+	// color:Colors.blue,
+	// transparent:true,
+	// opacity:.8,
+	// shading:THREE.FlatShading,
+  //
+  // });
+  //
+  // this.mesh = new THREE.Mesh(geom, mat);
+  // this.mesh.name = "waves";
+  // this.mesh.receiveShadow = true;
 
 }
 
-Sea.prototype.moveWaves = function (){
-  var verts = this.mesh.geometry.vertices;
-  var l = verts.length;
-  for (var i=0; i<l; i++){
-	var v = verts[i];
-	var vprops = this.waves[i];
-	v.x =  vprops.x + Math.cos(vprops.ang)*vprops.amp;
-	v.y = vprops.y + Math.sin(vprops.ang)*vprops.amp;
-	vprops.ang += vprops.speed*deltaTime;
-	this.mesh.geometry.verticesNeedUpdate=true;
-  }
-}
+// Sea.prototype.moveWaves = function (){
+//   var verts = this.mesh.geometry.vertices;
+//   var l = verts.length;
+//   for (var i=0; i<l; i++){
+// 	var v = verts[i];
+// 	var vprops = this.waves[i];
+// 	v.x =  vprops.x + Math.cos(vprops.ang)*vprops.amp;
+// 	v.y = vprops.y + Math.sin(vprops.ang)*vprops.amp;
+// 	vprops.ang += vprops.speed*deltaTime;
+// 	this.mesh.geometry.verticesNeedUpdate=true;
+//   }
+// }
 
 
 var Sky = function(){
